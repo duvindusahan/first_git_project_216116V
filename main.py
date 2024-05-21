6bd8b51c2899af86e711b0a6d6bd8976fcf3baa1
duvindusahan <duvindusahan@gmail.com>
2024-05-21 19:36:38 +0530

    Added helper.py and used helper_function in main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from helper import helper_function
+
 def greet():
     return "Hello, World!"
 
@@ -7,3 +9,4 @@ def farewell():
 if __name__ == "__main__":
     print(greet())
     print(farewell())
+    print(helper_function())
